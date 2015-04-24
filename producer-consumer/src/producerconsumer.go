9845344569923cf4b0959ddd58a4a67074cd8d64
david.kaspar <david.kaspar@gamesys.co.uk>
2015-04-24 15:01:04 +0100

    basic messaging

--- producer-consumer/src/producerconsumer.go
+++ producer-consumer/src/producerconsumer.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+    "fmt"
+)
+
 func main() {
+
+    messages := make(chan string)
+
+    go consumer(messages)
+    go producer(messages)
+
+    var input string
+    fmt.Scanln(&input)
+    fmt.Println("Done...")
+}
+
+func consumer(messages chan string) {
+    messages <- "hello"
+}
+
+func producer(messages chan string) {
+    msg := <-messages
+    fmt.Println(msg)
 }
