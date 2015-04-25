f2a236f6ef84f006c6ed7cb5509c14451b4bba6c
david.kaspar <david.kaspar@gamesys.co.uk>
2015-04-25 13:33:25 +0100

    moved starting of go routines into the methods rather than driven from main methods create and return channels rathern than driven from main use struct as type in channels rather than boolean since type doesn't matter

--- producer-consumer/src/producerconsumer.go
+++ producer-consumer/src/producerconsumer.go
@@ -11,57 +11,77 @@ func main() {
     //arguments
     producers := 10
 
-    messages := make(chan string)
-    done := make(chan bool)
-
     //start single consumer
-    go consumer(messages)
+    messages, stopConsumer := startConsumer()
 
     //start x producers
     var wg sync.WaitGroup
     wg.Add(producers)
-    for i:=0;i<producers;i++ {
-        go producer(messages,fmt.Sprintf("producer%d",i),done,wg)
+    stopProducers := make([]chan struct {},0)
+    for i := 0; i<producers; i++ {
+        stopProducer := startProducer(messages, fmt.Sprintf("producer%d", i), wg)
+        stopProducers = append(stopProducers, stopProducer)
     }
 
     //Run the show for 1 second
     time.Sleep(time.Second)
 
     //send done signal to x producers
-    for i:=0;i<producers;i++ {
-        done <- true
+    for _, stopProducer := range stopProducers {
+        stopProducer <- struct{}{}
     }
 
     //wait for all producers to finish
     wg.Wait()
-    close(messages)
-    close(done)
+    stopConsumer <- struct {}{}
+
     fmt.Println("Done...")
 }
 
-func consumer(messages chan string) {
+func startConsumer() (chan string, chan struct{}) {
 
-    for {
-        msg, more := <-messages
-        if more {
-            fmt.Println(msg)
-        } else {
-            fmt.Println("No more messages, press any key to terminate...")
-            return
+    //channels to communicate messages and stop signal
+    messages := make(chan string)
+    stopConsumer := make(chan struct {})
+
+    //results map
+    counter := make(map[string]int)
+
+    //start go routine
+    go func() {
+        for {
+            select {
+            case msg := <-messages :
+                counter[msg]++
+            case <-stopConsumer :
+                fmt.Println("Received consumer done signal...")
+                return
+            }
         }
-    }
+    }()
+
+    return messages, stopConsumer
 }
 
-func producer(messages chan string, name string, done chan bool, wg sync.WaitGroup) {
-    isDone:=false
+func startProducer(messages chan string, name string, wg sync.WaitGroup) chan struct {} {
+
+    //channel to signal to stop this producer
+    stopProducer := make(chan struct {})
+
+    //defer decreasing workgroup counter until exit
     defer wg.Done()
-    for i := 0;isDone==false; i++ {
-        select {
-        case isDone = <- done :
+
+    go func() {
+        for {
+            select {
+            case <-stopProducer :
                 fmt.Println("Received done signal...")
                 return
-        default :
-            messages <- fmt.Sprintf("%s-%d", name, i)
+            default :
+                messages <- name
+            }
         }
-    }
+    }()
+
+    return stopProducer
 }
