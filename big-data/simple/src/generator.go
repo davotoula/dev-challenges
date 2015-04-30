04aa27f3b4c001bc5003490a97f4545a036d04bd
david.kaspar <david.kaspar@gamesys.co.uk>
2015-04-30 09:48:45 +0100

    updated generator to take filename as optional parameter

--- big-data/simple/src/generator.go
+++ big-data/simple/src/generator.go
@@ -17,20 +17,24 @@ func main() {
 
     filePath := "transactions_gen.csv"
 
-    if (len(os.Args)!=3) {
-        fmt.Printf("=== Usage %s [number of partners, eg. 1000] [number of total transactions, eg. 1000000] [partner to calculate total for]\n", os.Args[0])
+    if (len(os.Args)<3) {
+        fmt.Printf("=== Usage %s [number of partners, eg. 1000] [number of total transactions, eg. 1000000] [output file, defaults to transactions_gen.csv]\n", os.Args[0])
         os.Exit(0)
     }
 
     numPartners,_ := strconv.Atoi(os.Args[1])
     numTransactions,_ := strconv.Atoi(os.Args[2])
 
+	if (len(os.Args[3]) > 0) {
+		filePath = os.Args[3]
+	}
+
     f, err := os.Create(filePath)
     check(err)
     defer f.Close()
 
     w := bufio.NewWriter(f)
-    for i:=0; i<numTransactions; i++ {
+    for i:=1; i<=numTransactions; i++ {
         transactionString := fmt.Sprintf("Partner %d,GBP,%f\n",rand.Intn(numPartners),rand.Float32()*1000)
         _, err := w.WriteString(transactionString)
         check(err)
