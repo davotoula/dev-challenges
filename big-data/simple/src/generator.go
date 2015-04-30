7333b11a1b86fcd394d767156547d50964837292
david.kaspar <david.kaspar@gamesys.co.uk>
2015-04-30 09:51:19 +0100

    applied go fmt

--- big-data/simple/src/generator.go
+++ big-data/simple/src/generator.go
@@ -1,47 +1,48 @@
 package main
+
 import (
-    "os"
-    "bufio"
-    "fmt"
-    "math/rand"
-    "strconv"
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
 )
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func main() {
 
-    filePath := "transactions_gen.csv"
+	filePath := "transactions_gen.csv"
 
-    if (len(os.Args)<3) {
-        fmt.Printf("=== Usage %s [number of partners, eg. 1000] [number of total transactions, eg. 1000000] [output file, defaults to transactions_gen.csv]\n", os.Args[0])
-        os.Exit(0)
-    }
+	if len(os.Args) < 3 {
+		fmt.Printf("=== Usage %s [number of partners, eg. 1000] [number of total transactions, eg. 1000000] [output file, defaults to transactions_gen.csv]\n", os.Args[0])
+		os.Exit(0)
+	}
 
-    numPartners,_ := strconv.Atoi(os.Args[1])
-    numTransactions,_ := strconv.Atoi(os.Args[2])
+	numPartners, _ := strconv.Atoi(os.Args[1])
+	numTransactions, _ := strconv.Atoi(os.Args[2])
 
-	if (len(os.Args[3]) > 0) {
+	if len(os.Args[3]) > 0 {
 		filePath = os.Args[3]
 	}
 
-    f, err := os.Create(filePath)
-    check(err)
-    defer f.Close()
+	f, err := os.Create(filePath)
+	check(err)
+	defer f.Close()
 
-    w := bufio.NewWriter(f)
-    for i:=1; i<=numTransactions; i++ {
-        transactionString := fmt.Sprintf("Partner %d,GBP,%f\n",rand.Intn(numPartners),rand.Float32()*1000)
-        _, err := w.WriteString(transactionString)
-        check(err)
-        if (i%10000==0) {
-            fmt.Println(i)
-        }
-    }
+	w := bufio.NewWriter(f)
+	for i := 1; i <= numTransactions; i++ {
+		transactionString := fmt.Sprintf("Partner %d,GBP,%f\n", rand.Intn(numPartners), rand.Float32()*1000)
+		_, err := w.WriteString(transactionString)
+		check(err)
+		if i%10000 == 0 {
+			fmt.Println(i)
+		}
+	}
 
-    w.Flush()
+	w.Flush()
 }
--- big-data/simple/src/summariser.go
+++ big-data/simple/src/summariser.go
@@ -1,96 +1,97 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "encoding/csv"
-    "strconv"
-    "io"
-    "time"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"time"
 )
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func main() {
-    start := time.Now()
+	start := time.Now()
 
-    //input args java -jar Aggregator.jar transactions.csv exchangerates.csv "Defence ltd." GBP
-    if (len(os.Args)!=5) {
-        fmt.Printf("=== Usage %s [transactions file path] [exchange rates file path] [partner to calculate total for] [home currency]\n", os.Args[0])
-        os.Exit(0)
-    }
+	//input args java -jar Aggregator.jar transactions.csv exchangerates.csv "Defence ltd." GBP
+	if len(os.Args) != 5 {
+		fmt.Printf("=== Usage %s [transactions file path] [exchange rates file path] [partner to calculate total for] [home currency]\n", os.Args[0])
+		os.Exit(0)
+	}
 
-    transactionsFilePath := os.Args[1]
-    exchangeRatesFilePath := os.Args[2]
-    partner := os.Args[3]
-    homeCurrency := os.Args[4]
+	transactionsFilePath := os.Args[1]
+	exchangeRatesFilePath := os.Args[2]
+	partner := os.Args[3]
+	homeCurrency := os.Args[4]
 
+	//result object
+	aggregatedTransactions := make(map[string]float32)
 
-    //result object
-    aggregatedTransactions := make(map[string]float32)
+	//load rates into map
+	fmt.Println("Loading exchange rates...")
+	exchangeRates := loadExchangeRates(exchangeRatesFilePath)
 
-    //load rates into map
-    fmt.Println("Loading exchange rates...")
-    exchangeRates := loadExchangeRates(exchangeRatesFilePath)
+	//load transactions one line at a time and start aggregating results
+	fmt.Printf("Calculating partner totals for [%s]...\n", partner)
+	csvfile, err := os.Open(transactionsFilePath)
+	check(err)
+	defer csvfile.Close()
 
-    //load transactions one line at a time and start aggregating results
-    fmt.Printf("Calculating partner totals for [%s]...\n",partner)
-    csvfile, err := os.Open(transactionsFilePath)
-    check(err)
-    defer csvfile.Close()
+	reader := csv.NewReader(csvfile)
+	reader.FieldsPerRecord = 3 // Expected records per line
 
-    reader := csv.NewReader(csvfile)
-    reader.FieldsPerRecord = 3 // Expected records per line
+	for i := 1; ; i++ {
+		transactionLine, err := reader.Read() //Reaad one line at a time
 
-    for i:=1;;i++ {
-        transactionLine, err := reader.Read() //Reaad one line at a time
+		if (err != nil) && (err == io.EOF) {
+			break
+		}
 
-        if ((err != nil)&&(err == io.EOF)) {break}
+		partnerName := transactionLine[0]
+		aggregatedTransactions[partnerName] += convertToHomeAmount(homeCurrency, exchangeRates, transactionLine)
 
-        partnerName := transactionLine[0]
-        aggregatedTransactions[partnerName] += convertToHomeAmount(homeCurrency, exchangeRates, transactionLine)
+		if i%1000000 == 0 {
+			fmt.Println(i, "records processed...")
+		}
+	}
 
-        if(i%1000000==0) {
-            fmt.Println(i, "records processed...")
-        }
-    }
+	//write aggregated transactions to disk
+	writeMapToDiskAsCsv(aggregatedTransactions)
 
-    //write aggregated transactions to disk
-    writeMapToDiskAsCsv(aggregatedTransactions)
+	//output aggregated total for specified partner to console
+	fmt.Printf("%.02f (for partner %s and currency %s)\n", aggregatedTransactions[partner], partner, homeCurrency)
 
-    //output aggregated total for specified partner to console
-    fmt.Printf("%.02f (for partner %s and currency %s)\n",aggregatedTransactions[partner], partner, homeCurrency)
-
-    elapsed := time.Since(start)
-    fmt.Printf("Excution took %s\n", elapsed)
+	elapsed := time.Since(start)
+	fmt.Printf("Excution took %s\n", elapsed)
 }
 
 func convertToHomeAmount(homeCurrency string, exchangeRates map[Key]float32, transactionLine []string) float32 {
-    transactionCurrency := transactionLine[1]
-    transactionAmount,_ := strconv.ParseFloat(transactionLine[2],32)
-    if (transactionCurrency==homeCurrency) {
-        return float32(transactionAmount)
-    } else {
-        return float32(transactionAmount)*exchangeRates[Key{transactionCurrency, homeCurrency}]
-    }
+	transactionCurrency := transactionLine[1]
+	transactionAmount, _ := strconv.ParseFloat(transactionLine[2], 32)
+	if transactionCurrency == homeCurrency {
+		return float32(transactionAmount)
+	} else {
+		return float32(transactionAmount) * exchangeRates[Key{transactionCurrency, homeCurrency}]
+	}
 }
 
 func writeMapToDiskAsCsv(records map[string]float32) {
-    csvfile, err := os.Create("aggregated_transactions_by_partner.csv")
-    check(err)
-    defer csvfile.Close()
+	csvfile, err := os.Create("aggregated_transactions_by_partner.csv")
+	check(err)
+	defer csvfile.Close()
 
-    for k, v := range records {
+	for k, v := range records {
 
-        _,err = csvfile.WriteString(fmt.Sprintf("%s,%.02f\n",k,v))
-        if err != nil {
-            panic(err)
-        }
-    }
+		_, err = csvfile.WriteString(fmt.Sprintf("%s,%.02f\n", k, v))
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 //load exchange rates into map of maps
@@ -104,28 +105,28 @@ func writeMapToDiskAsCsv(records map[string]float32) {
 //        ...
 
 type Key struct {
-    FromCurrency string
-    ToCurrency string
+	FromCurrency string
+	ToCurrency   string
 }
 
 func loadExchangeRates(filePath string) map[Key]float32 {
-    csvfile, err := os.Open(filePath)
-    check(err)
-    defer csvfile.Close()
+	csvfile, err := os.Open(filePath)
+	check(err)
+	defer csvfile.Close()
 
-    reader := csv.NewReader(csvfile)
-    reader.FieldsPerRecord = 3 // Expected records per line
+	reader := csv.NewReader(csvfile)
+	reader.FieldsPerRecord = 3 // Expected records per line
 
-    rawCSVdata, err := reader.ReadAll() //Read all at once
-    check(err)
+	rawCSVdata, err := reader.ReadAll() //Read all at once
+	check(err)
 
-    exchangeRates := make(map[Key]float32)
-    for _, each := range rawCSVdata {
-        fromCurrency := each[0]
-        toCurrency := each[1]
-        exchangeRate, _ := strconv.ParseFloat(each[2], 32)
-        exchangeRates[Key{fromCurrency, toCurrency}] = float32(exchangeRate)
-    }
+	exchangeRates := make(map[Key]float32)
+	for _, each := range rawCSVdata {
+		fromCurrency := each[0]
+		toCurrency := each[1]
+		exchangeRate, _ := strconv.ParseFloat(each[2], 32)
+		exchangeRates[Key{fromCurrency, toCurrency}] = float32(exchangeRate)
+	}
 
-    return exchangeRates
+	return exchangeRates
 }
