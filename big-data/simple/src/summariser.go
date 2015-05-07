5ccaea79ddbf7cd6d5d573a9e414125e1be1b35d
david.kaspar <david.kaspar@gamesys.co.uk>
2015-05-07 18:15:37 +0100

    reading in seperate go routine

--- big-data/simple/src/summariser.go
+++ big-data/simple/src/summariser.go
@@ -10,12 +10,19 @@ import (
 	"flag"
 	"log"
 	"runtime/pprof"
+	"runtime"
 )
 
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+type TransactionLine struct {
+	PartnerName string
+	Currency string
+	Amount float64
 }
 
 func main() {
@@ -25,6 +32,7 @@ func main() {
 	exchangeRatesFilePath := flag.String("ef", "exchangerates.csv", "exchange rates file")
 	partner := flag.String("partner", "Partner 1", "the partner name to group by")
 	homeCurrency := flag.String("hc", "GBP", "Home currency to use")
+	cores := flag.Int("cores", 1, "Amount of cores to use")
 
 	//parse for input arguments
 	flag.Parse()
@@ -39,11 +47,12 @@ func main() {
 		defer pprof.StopCPUProfile()
 	}
 
+	runtime.GOMAXPROCS(*cores)
 
 	start := time.Now()
 
 	//result object
-	aggregatedTransactions := make(map[string]float32,200)
+	aggregatedTransactions := make(map[string]float64,200)
 
 	//load rates into map
 	fmt.Println("Loading exchange rates...")
@@ -51,27 +60,49 @@ func main() {
 
 	//load transactions one line at a time and start aggregating results
 	fmt.Printf("Calculating partner totals for [%s]...\n", *partner)
-	csvfile, err := os.Open(*transactionsFilePath)
-	check(err)
-	defer csvfile.Close()
 
-	reader := csv.NewReader(csvfile)
-	reader.FieldsPerRecord = 3 // Expected records per line
+	readerOut := make(chan TransactionLine, 100000)
+	go func(out chan TransactionLine) {
 
+		csvfile, err := os.Open(*transactionsFilePath)
+		check(err)
+		defer csvfile.Close()
+		defer close(out)
+
+		reader := csv.NewReader(csvfile)
+		reader.FieldsPerRecord = 3 // Expected records per line
+
+		for i := 1; ; i++ {
+			transactionLineStrings, err := reader.Read() //Read one line at a time
+
+			if (err != nil) && (err == io.EOF) {
+				break
+			}
+			amountFloat, _ := strconv.ParseFloat(transactionLineStrings[2], 64)
+			out <- TransactionLine{PartnerName:transactionLineStrings[0], Currency:transactionLineStrings[1], Amount:amountFloat}
+		}
+
+		fmt.Println("Finishing go routine...")
+
+	}(readerOut)
+
+
+	//receive lines until channel is closed
 	for i := 1; ; i++ {
-		transactionLine, err := reader.Read() //Reaad one line at a time
+		transactionLine, more := <-readerOut
+		if more {
 
-		if (err != nil) && (err == io.EOF) {
+			aggregatedTransactions[transactionLine.PartnerName] += convertToHomeAmount(*homeCurrency, exchangeRates, transactionLine.Currency, transactionLine.Amount)
+
+			if i%1000000 == 0 {
+				fmt.Println(i, "records processed...")
+			}
+		} else {
+			fmt.Println("Channel closed, breaking...")
 			break
 		}
-
-		partnerName := transactionLine[0]
-		aggregatedTransactions[partnerName] += convertToHomeAmount(*homeCurrency, exchangeRates, transactionLine)
-
-		if i%1000000 == 0 {
-			fmt.Println(i, "records processed...")
-		}
 	}
+
 
 	//write aggregated transactions to disk
 	writeMapToDiskAsCsv(aggregatedTransactions)
@@ -83,17 +114,15 @@ func main() {
 	fmt.Printf("Excution took %s\n", elapsed)
 }
 
-func convertToHomeAmount(homeCurrency string, exchangeRates map[Key]float32, transactionLine []string) float32 {
-	transactionCurrency := transactionLine[1]
-	transactionAmount, _ := strconv.ParseFloat(transactionLine[2], 32)
+func convertToHomeAmount(homeCurrency string, exchangeRates map[Key]float64, transactionCurrency string, transactionAmount float64) float64 {
 	if transactionCurrency == homeCurrency {
-		return float32(transactionAmount)
+		return transactionAmount
 	} else {
-		return float32(transactionAmount) * exchangeRates[Key{transactionCurrency, homeCurrency}]
+		return transactionAmount * exchangeRates[Key{transactionCurrency, homeCurrency}]
 	}
 }
 
-func writeMapToDiskAsCsv(records map[string]float32) {
+func writeMapToDiskAsCsv(records map[string]float64) {
 	csvfile, err := os.Create("aggregated_transactions_by_partner.csv")
 	check(err)
 	defer csvfile.Close()
@@ -122,7 +151,7 @@ type Key struct {
 	ToCurrency   string
 }
 
-func loadExchangeRates(filePath string) map[Key]float32 {
+func loadExchangeRates(filePath string) map[Key]float64 {
 	csvfile, err := os.Open(filePath)
 	check(err)
 	defer csvfile.Close()
@@ -133,12 +162,12 @@ func loadExchangeRates(filePath string) map[Key]float32 {
 	rawCSVdata, err := reader.ReadAll() //Read all at once
 	check(err)
 
-	exchangeRates := make(map[Key]float32,100)
+	exchangeRates := make(map[Key]float64,100)
 	for _, each := range rawCSVdata {
 		fromCurrency := each[0]
 		toCurrency := each[1]
-		exchangeRate, _ := strconv.ParseFloat(each[2], 32)
-		exchangeRates[Key{fromCurrency, toCurrency}] = float32(exchangeRate)
+		exchangeRate, _ := strconv.ParseFloat(each[2], 64)
+		exchangeRates[Key{fromCurrency, toCurrency}] = exchangeRate
 	}
 
 	return exchangeRates
